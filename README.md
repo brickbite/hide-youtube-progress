625afcf9d9abff35291298cc24721538ad507963
brickbite <jeffreylee0116@gmail.com>
2021-06-12 15:44:54 -0700

    more improvements
    - hides video lengths on player endscreen
    - cleans up selectors
    - keep MutationObserver active for the lifecycle of the page and adds check for nodeType

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # hide-youtube-progress
 Browser Extension to Hide Youtube Progress
 
-Note: This extension is currently in developer mode only, and is not yet published.
+Note: This extension is currently in developer mode, and is not yet published.
 
-# Using this extension
+## Background
+
+When watching vods of matches, the video's duration is usually shown. This can be telling of the result, since video length can correlate to how close or one-sided the match was. By hiding the progress bar and video duration, it brings the watching experience closer to that of a live stream.
+
+# Installing the extension
 
 ## Chrome:
 1. navigate to `chrome://extensions` - turn on developer mode at the top right
@@ -12,6 +16,23 @@ Note: This extension is currently in developer mode only, and is not yet publish
 ## Mozilla Firefox:
 1. navigate to `about:debugging#/runtime/this-firefox`
 2. `Load Temporary Add-on`, select the `manifest.json` file in the /firefox directory
+
+# Using the extension
+
+## extension icon
+- in your browser's extension toolbar, click the extension to open a popup. there is a checkbox that controls the initial state of newly opened / refreshed youtube pages.
+- Note: changing the checkbox here does not affect any youtube pages that are already open
+
+## on the youtube player
+- on a page with a youtube video, the timestamp in the controls becomes clickable after the page loads. clicking this will toggle the visibility of relevant page elements: progress bar, total time, timestamps on thumbnails, timestamps on endscreen thumbnails
+- hotkey `s` anywhere on the page does the same thing as above
+
+## on the youtube homepage
+- when set to hide video times, video times on the youtube homepage are also hidden
+- there is no video player here, but the hotkey `s` also works here
+
+## other areas
+- not yet tested
 
 # Version History
 
@@ -24,9 +45,10 @@ Note: This extension is currently in developer mode only, and is not yet publish
 - updates contentScript to check against browser storage on page load / refresh
 - adds html / css / js / images for popup - controls initial variable in browser storage
 - handles promises when getting from browser storage in contentScript
-- adds check for mutation.type back to MutationObserver
-- changes global toggle slider to a simpler checkbox
-- removes toggle button on player with a click handler on time display
+- adds checks to MutationObserver for performance
+- hides video lengths on player endscreen
+- cleans up selectors
+- keeps MutationObserver active for the lifecycle of the page
 
 ## v0.1 - 2021-05-24
 - initial proof of concept
--- firefox/contentScript.js
+++ firefox/contentScript.js
@@ -8,6 +8,13 @@ const windowVarName = '__hideYtTimes';
 const storageName = 'hideYtTimes';
 const toggleButtonId = 'time-display-show';
 
+// relevant youtube selectors. ytp is youtube player
+const ytpTimeDisplaySelector = 'div.ytp-time-display';
+const ytpTotalTimeSelector = 'span.ytp-time-duration';
+const ytpProgressBarSelector = 'div.ytp-progress-bar-container';
+const ytpEndscreenTimeSelector = 'span.ytp-videowall-still-info-duration';
+const thumbnailTimeSelector = 'ytd-thumbnail-overlay-time-status-renderer';
+
 function logError(error) {
   console.error(error);
 }
@@ -37,11 +44,17 @@ function updateElementVisibility(element) {
     return;
   }
 
-  // youtube scripts edit visibility on actions like hover, but they don't use display, so we use display here to show / hide elements
+  // youtube page script edits visibility on hovering a video list thumbnail, and edits display on hovering endscreen thumbnail
+
+  // to cover the various cases, we apply display, visibility, and opacity redundantly
   if (window[windowVarName] === true) {
     element.style.display = 'none';
+    element.style.visibility = 'hidden';
+    element.style.opacity = '0';
   } else if (window[windowVarName] === false) {
     element.style.display = 'inline';
+    element.style.visibility = 'visible';
+    element.style.opacity = '1';
   }
 }
 
@@ -68,37 +81,23 @@ function selectAndUpdateElement(selector) {
 }
 
 /*****************
- * remove timestamps while browsing lists (thumbnails)
+ * remove times from thumbnails while browsing lists and player endscreen
  *****************/
 function updateThumbnailTimestamps() {
-  const timestampId = '#overlays';
+  const thumbnailTimes = [...document.querySelectorAll(thumbnailTimeSelector)];
+  thumbnailTimes.forEach(updateElementVisibility);
 
-  const timestamps = [...document.querySelectorAll(timestampId)];
-  timestamps.forEach(updateElementVisibility);
+  const endscreenTimes = [
+    ...document.querySelectorAll(ytpEndscreenTimeSelector),
+  ];
+  endscreenTimes.forEach(updateElementVisibility);
 }
 
 /*****************
- * remove relevant things on video player page
+ * remove singular elements on video player
  *****************/
 function updateVideoPlayerProgress() {
-  // TODO: simplify selectors in this function
-
-  // const parentSelector = '#movie_player > div.ytp-chrome-bottom >'
-  const progressBarSelector = 'div.ytp-progress-bar-container';
-
-  const totalTimeSelector =
-    'div.ytp-chrome-controls > div.ytp-left-controls > div.ytp-time-display.notranslate > span.ytp-time-duration';
-
-  const thumbnailTimeSelector =
-    '#overlays > ytd-thumbnail-overlay-time-status-renderer';
-
-  // TODO: add selector for thumbnails at video end?
-
-  const playerSelectors = [
-    progressBarSelector,
-    totalTimeSelector,
-    thumbnailTimeSelector,
-  ];
+  const playerSelectors = [ytpProgressBarSelector, ytpTotalTimeSelector];
 
   playerSelectors.forEach(selectAndUpdateElement);
 
@@ -107,7 +106,7 @@ function updateVideoPlayerProgress() {
   if (!toggleButton) {
     return;
   }
-  
+
   updateToggleButtonVisibility(toggleButton);
 }
 
@@ -131,11 +130,8 @@ function toggleHideShow() {
 /*****************
  * adds toggle control to the youtube player's time display
  *****************/
- function addTimeControlHandler() {
-  const timeDisplaySelector =
-    'div.ytp-chrome-controls > div.ytp-left-controls > div.ytp-time-display';
-
-  const ytTimeDisplay = document.querySelector(timeDisplaySelector);
+function addTimeControlHandler() {
+  const ytTimeDisplay = document.querySelector(ytpTimeDisplaySelector);
 
   if (!ytTimeDisplay) {
     return;
@@ -168,9 +164,16 @@ const config = { attributes: true, childList: true, subtree: true };
 // Create an observer instance
 const observer = new MutationObserver((mutationsList, observer) => {
   for (const mutation of mutationsList) {
-    // TODO: add more exit conditions if the element is not relevant
     if (mutation.type !== 'childList') {
       return;
+    }
+
+    for (const node of mutation.addedNodes) {
+      if (node.nodeType !== Node.ELEMENT_NODE) {
+        return;
+      }
+
+      // TODO: add further exit conditions if node contains no relevant selectors
     }
 
     updateVisual();
@@ -179,21 +182,15 @@ const observer = new MutationObserver((mutationsList, observer) => {
 
 // run this at beginning to set the default variable
 setInitialWindowVar().then(() => {
+  // this continues to observe while we're on the page, so any new content (added by actions such as scrolling) will be correctly shown / hidden
   observer.observe(targetNode, config);
 }, logError);
 
-// stop observing and add manual controls after page has loaded
+// add manual controls after page has loaded
 window.onload = (event) => {
-  console.log('page is fully loaded');
+  console.log('page is loaded');
 
-  // disconnect observer for toggling performance after page has loaded
-  // Note: when disconnecting here, user can scroll down and be spoiled by newly loaded video thumbnails? or comments (not hidden by this extension)?
-  // TODO: investigate: .disconnect() seems to run before times are rendered on youtube homepage. find a event listener better than window.onload
-  setTimeout(() => {
-    observer.disconnect();
-  }, 3000);
-
-  // TODO: append toggle button as soon as player loads (instead of when page is done loading)
+  // TODO: append toggle button on player load (instead of after page load)
   addTimeControlHandler();
 
   // hotkey `s` for same behavior as button
@@ -202,4 +199,8 @@ window.onload = (event) => {
       toggleHideShow();
     }
   };
+};
+
+window.onbeforeunload = (event) => {
+  observer.disconnect();
 };
